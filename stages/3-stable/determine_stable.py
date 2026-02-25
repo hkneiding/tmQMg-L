fcc60aaa515c3638c6d6c0b29173fb933be4d565
Hannes Kneiding <k.1996@live.de>
2026-02-25 10:17:45 +0100

    fix bug in the determination of stable occurrences

--- stages/3-stable/determine_stable.py
+++ stages/3-stable/determine_stable.py
@@ -10,18 +10,23 @@ stable_data = []
 for ligand in ligands_misc.to_dict(orient="records"):
 
     lowest_energy = None
+    lowest_energy_subgraph_name = None
     for subgraph_name in eval(ligand['metal_bond_node_idx_groups']).keys():
 
-        if not subgraph_name in single_point_summary['occurrence']:
+        if not subgraph_name in single_point_summary['occurrence'].tolist():
             continue
 
-        subgraph_energy = single_point_summary.loc[single_point_summary['occurrence'] == subgraph_name]['energy']
+        subgraph_energy = single_point_summary.loc[single_point_summary['occurrence'] == subgraph_name]['energy'].item()
         if lowest_energy is None or subgraph_energy < lowest_energy:
             lowest_energy = subgraph_energy
-    
+            lowest_energy_subgraph_name = subgraph_name
+
+    if lowest_energy is None:
+        continue
+
     stable_data.append({
         'name': ligand['name'],
-        'stable_occurrence': subgraph_name
+        'stable_occurrence': lowest_energy_subgraph_name
     })
 
 pd.DataFrame(stable_data).to_csv('stable.csv', index=False, sep=';')
